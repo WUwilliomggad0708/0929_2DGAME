09ea45d26cd69f13515b48eef34a7009bde3729a
WUwilliomggad0708 <wuwilliomggad@gmail.com>
2022-12-01 16:05:51 +0800

    bullet

---
+++ 0929/Assets/Scripts/FireSystemBase.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace xin
+{
+    public class FireSystemBase : MonoBehaviour
+    {
+
+        [SerializeField, Header("子彈預置物")]
+        private GameObject prefabBullet;
+        [SerializeField, Header("子彈生成點")]
+        private Transform pointSpawn;
+
+    }
+}
