fc3c1d472d6e9e0f4dec40734b3715e91b1b8260
WUwilliomggad0708 <wuwilliomggad@gmail.com>
2022-12-01 19:44:43 +0800

    Falt
    網頁設定 projectsetting裡解析度設定錯誤

--- 0929/Assets/Scripts/DamageSystem.cs
+++ 0929/Assets/Scripts/DamageSystem.cs
@@ -13,6 +13,26 @@ namespace xin
         private void OnCollisionEnter2D(Collision2D collision)
         {
             print(collision.gameObject.name);
+            //如果碰到物件名稱 包含NAMETARGRT 就爆炸並刪除
+            if (collision.gameObject.name.Contains(nameTarget));
+            {
+                //生成爆炸預置物 座標與角度 跟此物件相同
+                Instantiate(prefabExplosion, transform.position, transform.rotation);
+
+                //刪除Destroy
+                //此物件gamepbject
+                Destroy(gameObject);
+            }
+        }
+        //碰撞離開時執行一次
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            
+        }
+        //碰撞時持續執行
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            
         }
 
 
